d279b278f7ca3ac2cff8d75c5c71f65a0b24ef44
suriyaprakash-89 <tn.suriyaprakash29@gmail.com>
2025-02-27 17:47:10 +0530

    ch-1

---
+++ summa.js
@@ -0,0 +1,75 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+/*
+app.get("/", async (req, res) => {
+    try {
+      console.log(req.body);
+      const response = await axios.get(
+        `https://official-joke-api.appspot.com/random_joke`
+      );
+      const result = response.data;
+      console.log(result);
+      res.render("summa.ejs", {
+        data: result[Math.floor(Math.random() * result.length)],
+      });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("summa.ejs", {
+        error: "No activities that match your criteria.",
+      });
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+*/
+
+app.get("/", async (req, res) => {
+    try {
+      const ini = "Click the button for RANDOM-JOKE !";
+      /*console.log(req.body);
+      const response = await axios.get(
+        `https://official-joke-api.appspot.com/random_joke`
+      );
+      const result = response.data;
+      console.log(result);*/
+      res.render("summa.ejs", {
+        ini:ini //result[Math.floor(Math.random() * result.length)],
+      });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("summa.ejs", {
+        error: "No activities that match your criteria.",
+      });
+    }
+  });
+  
+app.post("/submit",async (req, res)=>{
+  try {
+    console.log(req.body);
+    const response = await axios.get(
+      `https://official-joke-api.appspot.com/random_joke`
+    );
+    const result = response.data;
+    console.log(result);
+    res.render("summa.ejs", {
+      data: result //result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("summa.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+});
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
