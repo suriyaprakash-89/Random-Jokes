bc544036f64eee172a6c805bf9225b6f60717eec
suriyaprakash-89 <tn.suriyaprakash29@gmail.com>
2025-02-27 17:57:30 +0530

    Fixed port undefined issue

--- summa.js
+++ summa.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
